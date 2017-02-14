4bd7292c7ed6c8eecbacc874b6f2856f2c57a53c
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2017-02-14 10:36:39 -0600

    inicio del repositorio

---
+++ Scripts Mysql/creartablas.sql
@@ -0,0 +1,13 @@
+CREATE TABLE COLOR
+(
+    IDColor int,
+    Nombre  varchar(255),
+    Tono    varchar(255)
+)
+
+CREATE TABLE COLOR
+(
+    IDColor int,
+    Nombre  varchar(255),
+    Tono    varchar(255)
+)
