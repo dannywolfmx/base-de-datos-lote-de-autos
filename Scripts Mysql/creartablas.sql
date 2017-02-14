86344c6e5084e2f04eafcb0544f65f25e4ab04d9
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2017-02-14 10:51:15 -0600

    cambio en el diagrama entidad relacion

--- Scripts Mysql/creartablas.sql
+++ Scripts Mysql/creartablas.sql
@@ -1,13 +1,29 @@
-CREATE TABLE COLOR
+CREATE TABLE Automovil
 (
-    IDColor int,
-    Nombre  varchar(255),
-    Tono    varchar(255)
+    IDAutomovil     int,
+    Modelo          varchar(255),
+    Anio            varchar(255),
+    Tipo            varchar(255),
+    Precio          Float,
+    Costo           Float,
+    PuedeConsignar  int
 )
+
+
 
 CREATE TABLE COLOR
 (
-    IDColor int,
-    Nombre  varchar(255),
-    Tono    varchar(255)
+    IDColor         int,
+    Nombre          varchar(255),
+    Tono            varchar(255)
+)
+
+
+
+CREATE TABLE TipoProblema
+(
+    IDTipoProblema  int,
+    Descuento       Float,
+    Nombre          varchar(255),
+    Nivel
 )
